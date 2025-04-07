fa398667b5d6af95c85c6e8c9a43f967540de973
Jonas Östlund <uppfinnarjonas@gmail.com>
2025-04-07 21:57:00 +0200

    Make example smaller

--- examples/libtaxonomy-js/src/cljs/libtaxonomy/my_component.cljs
+++ examples/libtaxonomy-js/src/cljs/libtaxonomy/my_component.cljs
@@ -1,13 +1,4 @@
-(ns libtaxonomy.my-component
-  (:require ["react" :refer [useState]]))
-
-;; this needs to be private since non-component exports break vite react HMR
-(defonce ^:private x 10)
+(ns libtaxonomy.my-component)
 
 (defn MyComponent []
-  (let [[state setState] (useState 0)]
-    #jsx [:div "You clicked " state " times"
-          [:button {:onClick #(do
-                                (set! x (inc x))
-                                (setState (inc state)))}
-           "Click!!! me"]]))
+  :jsx [:div "MyComponent todo"])
